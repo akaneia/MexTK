b090de6dc39210576494aca05abf24951d4bc6eb
ploaj_the_otter <ploaj_the_otter@yahoo.com>
2020-04-10 18:42:28 -0500

    reworked more elf processing

--- MexFF/Program.cs
+++ MexFF/Program.cs
@@ -1,6 +1,7 @@
 ﻿using HSDRaw;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 
 namespace MexFF
@@ -9,8 +10,9 @@ namespace MexFF
     {
         static void Main(string[] args)
         {
+            // Parse Args
             List<Tuple<int, string>> itemInputs = new List<Tuple<int, string>>();
-            string input = "";
+            List<string> inputs = new List<string>();
             string output = "";
             string symbolName = "ftFunction";
             string datFile = null;
@@ -18,13 +20,19 @@ namespace MexFF
             bool injectDat = false;
             bool quiet = false;
             bool yesOverwrite = false;
+            bool disableWarnings = false;
+            bool clean = false;
 
             for (int i = 0; i < args.Length; i++)
             {
                 if (args[i] == "-i" && i + 1 < args.Length)
-                    input = args[i + 1];
+                {
+                    inputs.Add(args[i + 1]);
+                }
                 if (args[i] == "-ii" && i + 2 < args.Length)
+                {
                     itemInputs.Add(new Tuple<int, string>(int.Parse(args[i + 1]), args[i + 2]));
+                }
                 if (args[i] == "-o" && i + 1 < args.Length)
                     output = args[i + 1];
                 if (args[i] == "-s" && i + 1 < args.Length)
@@ -33,21 +41,27 @@ namespace MexFF
                     datFile = args[i + 1];
                 if (args[i] == "-ow")
                     yesOverwrite = true;
+                if (args[i] == "-w")
+                    disableWarnings = true;
+                if (args[i] == "-c")
+                    clean = true;
                 if (args[i] == "-t" && i + 1 < args.Length)
                     fightFuncTable = File.ReadAllLines(args[i + 1]);
                 if (args[i] == "-q")
                     quiet = true;
             }
 
-            if(!string.IsNullOrEmpty(input) && itemInputs.Count > 0)
+            // don't allow both item tables and normal tables
+            if(inputs.Count > 0 && itemInputs.Count > 0)
             {
                 Console.WriteLine("Only -i or -ii can be used at once, not both");
                 Console.ReadLine();
                 return;
             }
             
-            if (string.IsNullOrEmpty(input)
-                 && itemInputs.Count == 0
+            // print instruction and exit if no input
+            if ((inputs.Count == 0
+                 && itemInputs.Count == 0)
                 || args.Length == 0)
             {
                 PrintInstruction();
@@ -55,15 +69,18 @@ namespace MexFF
                 return;
             }
 
+            // check if we should inject dat
             if (!string.IsNullOrEmpty(datFile))
             {
                 output = datFile;
                 injectDat = true;
             }
 
+            // create output path if one isn't entered
             if (string.IsNullOrEmpty(output))
-                output = Path.Combine(Path.GetDirectoryName(input), Path.GetFileName(input).Replace(Path.GetExtension(input), ".dat"));
+                output = Path.Combine(Path.GetDirectoryName(inputs[0]), Path.GetFileName(inputs[0]).Replace(Path.GetExtension(inputs[0]), ".dat"));
             
+            // check if output already exists
             if (File.Exists(output) && !yesOverwrite)
             {
                 Console.WriteLine(output + " already exists, overwrite? (y/n)");
@@ -71,14 +88,11 @@ namespace MexFF
                     return;
             }
 
+            // compile functions
             HSDAccessor function = null;
 
             if (itemInputs.Count == 0)
-            {
-                //if (!CheckFileExists(input))
-                //    return;
-                function = CompileInput(input, fightFuncTable, quiet);
-            }
+                function = CompileInput(inputs.ToArray(), fightFuncTable, quiet, disableWarnings, clean);
             else
             {
                 function = new HSDAccessor() { _s = new HSDStruct(4) };
@@ -86,20 +100,18 @@ namespace MexFF
                 int count = 0;
                 foreach(var f in itemInputs)
                 {
-                    //if (!CheckFileExists(f.Item2))
-                    //    return;
-
                     count = Math.Max(count, f.Item1 + 1);
 
                     if (4 + 4 * (f.Item1 + 1) > function._s.Length)
                         function._s.Resize(4 + 4 * (f.Item1 + 1));
 
-                    var relocFunc = CompileInput(f.Item2, fightFuncTable, quiet);
+                    var relocFunc = CompileInput(new string[] { f.Item2 }, fightFuncTable, quiet, disableWarnings, clean);
                     function._s.SetReference(4 + 0x04 * f.Item1, relocFunc);
                 }
                 function._s.SetInt32(0x00, count);
             }
             
+            // create DAT file
             if (function != null)
             {
                 HSDRawFile f;
@@ -162,46 +174,61 @@ namespace MexFF
         /// <param name="fightFuncTable"></param>
         /// <param name="quiet"></param>
         /// <returns></returns>
-        private static HSDAccessor CompileInput(string input, string[] fightFuncTable, bool quiet)
+        private static HSDAccessor CompileInput(string[] inputs, string[] funcTable, bool quiet, bool disableWarnings, bool clean)
         {
-            var ext = Path.GetExtension(input).ToLower();
+            if (inputs.Length == 0)
+                return null;
 
-            Dictionary<string, uint> funcTable = null;
-            if(fightFuncTable != null)
+            if (Environment.OSVersion.Platform != PlatformID.Win32NT)
+                throw new Exception("Invalid platform " + Environment.OSVersion.Platform.ToString());
+
+            var devkitpath = Environment.GetEnvironmentVariable("DEVKITPPC").Replace("opt/", "");
+
+            if (string.IsNullOrEmpty(devkitpath))
+                throw new FileNotFoundException("DEVKITPPC path not set");
+
+            var gccPath = Path.Combine(devkitpath, "bin/powerpc-eabi-gcc.exe");
+
+            if (!File.Exists(gccPath))
+                throw new FileNotFoundException("powerpc-eabi-gcc.exe not found");
+
+            var ext = Path.GetExtension(inputs[0]).ToLower();
+            List<RelocELF> elfs = new List<RelocELF>();
+
+            foreach(var input in inputs)
             {
-                funcTable = new Dictionary<string, uint>();
-                for (int i = 0; i < fightFuncTable.Length; i++)
-                    funcTable.Add(fightFuncTable[i], (uint)i);
-            }
-            else
-            {
-                Console.WriteLine("No table input, overloading enabled");
+                if (ext.Equals(".o"))
+                    elfs.Add(new RelocELF(File.ReadAllBytes(input)));
+
+                if (ext.Equals(".c"))
+                {
+                    Process p = new Process();
+
+                    p.StartInfo.UseShellExecute = false;
+                    p.StartInfo.WorkingDirectory = Path.GetDirectoryName(input);
+                    p.StartInfo.RedirectStandardOutput = true;
+                    p.StartInfo.FileName = gccPath;
+                    p.StartInfo.Arguments = $"-MMD -MP -MF -Wall -DGEKKO -mogc -mcpu=750 -meabi -mhard-float -c \"{input}\" {(disableWarnings ? "-w" : "")} -O2";
+                    p.Start();
+
+                    p.WaitForExit();
+
+                    var outputPath = Path.Combine(Path.GetDirectoryName(input), Path.GetFileNameWithoutExtension(input) + ".o");
+
+                    if (p.ExitCode != 0 || !File.Exists(outputPath))
+                    {
+                        Console.WriteLine();
+                        throw new InvalidDataException($"{Path.GetFileName(input)} failed to compile, see output above for details");
+                    }
+                    
+                    elfs.Add(new RelocELF(File.ReadAllBytes(outputPath)));
+
+                    if (clean)
+                        File.Delete(outputPath);
+                }
             }
 
-            if (ext.Equals(".o"))
-            {
-                try
-                {
-                    return ELFTools.ELFToDAT(input, funcTable, quiet);
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine(e.ToString());
-                }
-            }
-
-            if (ext.Equals(".c"))
-            {
-                try
-                {
-                    return ELFTools.CToDAT(input, funcTable, quiet);
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine(e.ToString());
-                }
-            }
-            return null;
+            return RelocELF.GenerateFunctionDAT(elfs.ToArray(), funcTable, quiet);
         }
 
         /// <summary>
@@ -213,14 +240,16 @@ namespace MexFF
 
 Usage: mexff.exe (type) (options)
 Options:
-    -i (file(.c, .o(ELF))) : Input File (.c and .o (elf)) files are supported can have multiple inputs for 'it' type
-    -ii (item index) (file(.c, .o(ELF))) : Specific index used for building ft tables
-    -o (file.dat) : output dat file name
-    -d (file.dat) : dat file to inject symbol into (will not output a 'new' dat file)
-    -ow : overwrites files if it exists
-    -s (name) : symbol name (default is ftFunction for ft and itFunction for it)
-    -t (file.txt) : specify symbol table list
-    -q : Quiet Mode (Console doesn't print information)
+    -i (file.c)                          : Input File (.c and .o (elf)) files are supported can have multiple inputs for 'it' type
+    -ii (item index) (file(.c, .o(ELF))) : specify index used for building ftItem tables
+    -o (file.dat)                        : output dat filename
+    -d (file.dat)                        : dat file to inject symbol into
+    -ow                                  : overwrites files if it exists
+    -s (symbol)                          : symbol name (default is ftFunction for ft and itFunction for it)
+    -t (file.txt)                        : specify symbol table list
+    -q                                   : Quiet Mode (Console doesn't print information)
+    -w                                   : disable compilation warnings
+    -c                                   : clean build (deletes .o files after compilation)
 
 Ex: mexff.exe -i 'main.c' -o 'ftFunction.dat' -q
 Compile 'main.c' and outputs 'ftFunction.dat' in quiet mode
@@ -230,8 +259,7 @@ Creates function table with bomb.c with index 0 and arrow.c with index 3
 and injects it into PlMan.dat with the symbol name itFunction
 
 Note: in order to compile .c files you must 
-have DEVKITPPC installed with GameCube rules
-as well as have the environment path setup");
+have DEVKITPPC installed and have the environment path setup");
         }
     }
 }
--- MexFF/RelocELF.cs
+++ MexFF/RelocELF.cs
@@ -3,19 +3,16 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MexFF
 {
     public class RelocELF
     {
-        //C:\devkitPro/devkitPPC/bin/powerpc-eabi-gcc.exe -MMD -MP -MF -g -O2 -Wall -DGEKKO -mogc -mcpu=750 -meabi -mhard-float -w -c source/test.c -o test.o
-
         public class SymbolData
         {
             public string Symbol;
             public string SectionName;
+            public bool External;
             public byte[] Data;
             public List<RelocData> Relocations = new List<RelocData>();
 
@@ -194,6 +191,14 @@ namespace MexFF
 
                         foreach(var rel in relocations)
                             rel.Offset -= sym.st_value;
+
+                        SymbolSections[i].External = Sections[sym.st_shndx].sh_offset == 0;
+
+                        if (sym.st_size == 0)
+                        {
+                            symbolData = section.Data;
+                            //Console.WriteLine(section.Name + " " + sym.st_value + " " + sym.st_size + " " + section.Data.Length);
+                        }
                     }
 
                     SymbolSections[i].Symbol = r.ReadString((int)(symbolStringSection.sh_offset + sym.st_name), -1);
@@ -234,51 +239,173 @@ namespace MexFF
             }
             return relocA;
         }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="symbol"></param>
-        /// <returns></returns>
-        public bool GetSymbol(string symbol)
-        {
-            //var sym = 
-
-            /*if(sym != null)
-            {
-                Console.WriteLine(sym.Symbol + " " + sym.SectionName);
-                Console.WriteLine(sym.Data.Length.ToString("X8"));
-
-                Console.WriteLine(BitConverter.ToString(sym.Data).Replace("-", ""));
-
-                foreach (var d in sym.GetDependencies())
-                {
-                    Console.WriteLine(d.SectionName);
-                }
-                
-            }*/
-
-            return false;// SymbolSections.Find(e => e.Symbol.Equals(symbol, StringComparison.InvariantCultureIgnoreCase));
-        }
-    
+        
         /// <summary>
         /// 
         /// </summary>
         /// <returns></returns>
-        public HSDAccessor GenerateFunctionDAT(string[] functions)
+        public static HSDAccessor GenerateFunctionDAT(RelocELF[] elfFiles, string[] functions, bool quiet = false)
         {
+            // Grab Symbol Table Info
             Dictionary<int, SymbolData> data = new Dictionary<int, SymbolData>();
+            Queue<SymbolData> symbolQueue = new Queue<SymbolData>();
 
-            for(int i = 0; i < functions.Length; i++)
+            // Gather the root symbols needed for function table
+            for (int i = 0; i < functions.Length; i++)
             {
-                var sym = SymbolSections.Find(e => e.Symbol.Equals(functions[i], StringComparison.InvariantCultureIgnoreCase));
-                if(sym != null)
-                    data.Add(i, sym);
+                foreach(var elf in elfFiles)
+                {
+                    var sym = elf.SymbolSections.Find(e => e.Symbol.Equals(functions[i], StringComparison.InvariantCultureIgnoreCase));
+                    if (sym != null)
+                    {
+                        data.Add(i, sym);
+                        symbolQueue.Enqueue(sym);
+                    }
+                }
+            }
+
+            // Resolve External and Duplicate Symbols
+            Dictionary<SymbolData, SymbolData> SymbolRemapper = new Dictionary<SymbolData, SymbolData>();
+
+            // Get All Symbols and Dependencies
+            List<SymbolData> usedSymbols = new List<SymbolData>();
+            while (symbolQueue.Count > 0)
+            {
+                var orgsym = symbolQueue.Dequeue();
+                var sym = orgsym;
+
+                // check if already remapped
+                if (SymbolRemapper.ContainsKey(sym))
+                    continue;
+
+                // resolve external and remap
+                if (sym.External)
+                {
+                    foreach (var elf in elfFiles)
+                    {
+                        var externalSymbol = elf.SymbolSections.Where(e => e.Symbol.Equals(sym.Symbol) && !e.External).ToArray();
+
+                        if (externalSymbol.Length > 0)
+                        {
+                            SymbolRemapper.Add(sym, externalSymbol[0]);
+                            sym = externalSymbol[0];
+                            break;
+                        }
+                    }
+                    if (sym == orgsym)
+                        throw new Exception("Could not resolve external symbol " + sym.Symbol + " - " + sym.SectionName);
+                }
+
+                // resolve duplicates
+                if (!string.IsNullOrEmpty(sym.Symbol))
+                {
+                    bool duplicate = false;
+                    // check if a symbol with this name is already used
+                    foreach (var s in usedSymbols)
+                    {
+                        if (s.Symbol.Equals(sym.Symbol))
+                        {
+                            SymbolRemapper.Add(sym, s);
+                            duplicate = true;
+                            break;
+                        }
+                    }
+                    // if a duplicate is found, ignore this symbol
+                    if (duplicate)
+                        continue;
+                }
+
+                // add symbols
+                usedSymbols.Add(sym);
+
+                foreach(var v in sym.Relocations)
+                {
+                    if (!usedSymbols.Contains(v.Symbol) && !symbolQueue.Contains(v.Symbol))
+                        symbolQueue.Enqueue(v.Symbol);
+                }
+            }
+            
+
+            // remap relocation table
+            foreach (var v in usedSymbols)
+            {
+                for(int i = 0; i < v.Relocations.Count; i++)
+                {
+                    if (SymbolRemapper.ContainsKey(v.Relocations[i].Symbol))
+                        v.Relocations[i].Symbol = SymbolRemapper[v.Relocations[i].Symbol];
+
+                    if (!usedSymbols.Contains(v.Relocations[i].Symbol))
+                        throw new Exception("Missing Symbol " + v.Relocations[i].Symbol.Symbol + " " + v.Symbol);
+                }
+
             }
 
 
+            // Generate Function DAT
 
-            return null;
+            var function = new HSDAccessor() { _s = new HSDStruct(0x14) };
+
+            // Generate code section
+            Dictionary<SymbolData, long> dataToOffset = new Dictionary<SymbolData, long>();
+            using (MemoryStream code = new MemoryStream())
+            {
+                foreach(var v in usedSymbols)
+                {
+                    dataToOffset.Add(v, code.Length);
+                    code.Write(v.Data, 0, v.Data.Length);
+                }
+                function._s.SetReferenceStruct(0x00, new HSDStruct(code.ToArray()));
+            }
+
+            // Generate Function Table
+
+            HSDStruct functionTable = new HSDStruct(8);
+            var funcCount = 0;
+            foreach(var v in data)
+            {
+                functionTable.Resize(8 * (funcCount + 1));
+                functionTable.SetInt32(funcCount * 8, v.Key);
+                functionTable.SetInt32(funcCount * 8 + 4, (int)dataToOffset[v.Value]);
+                funcCount++;
+            }
+
+            function._s.SetReferenceStruct(0x0C, functionTable);
+            function._s.SetInt32(0x10, funcCount);
+
+            // Generate Relocation Table
+
+            HSDStruct relocationTable = new HSDStruct(0);
+            var relocCount = 0;
+            foreach(var v in usedSymbols)
+            {
+                if(v.Data.Length == 0)
+                {
+                    throw new Exception($"Error: {v.Symbol} length is {v.Data.Length.ToString("X")}");
+                }
+
+                if (!quiet)
+                {
+                    Console.WriteLine($"{v.Symbol,-30} {v.SectionName, -50} Offset: {dataToOffset[v].ToString("X8"), -16} Length: {v.Data.Length.ToString("X8")}");
+                    if(v.Relocations.Count > 0)
+                        Console.WriteLine($"\t {"Section:",-50} {"RelocType:",-20} {"FuncOffset:", -16} {"SectionOffset:"}");
+                }
+
+                foreach (var reloc in v.Relocations)
+                {
+                    if (!quiet)
+                        Console.WriteLine($"\t {reloc.Symbol.SectionName, -50} {reloc.Type, -20} {reloc.Offset.ToString("X8"), -16} {reloc.AddEnd.ToString("X8")}");
+                    relocationTable.Resize((relocCount + 1) * 0x08);
+                    relocationTable.SetInt32(0x00 + relocCount * 8, (int)(dataToOffset[v] + reloc.Offset));
+                    relocationTable.SetByte(0x00 + relocCount * 8, (byte)reloc.Type);
+                    relocationTable.SetInt32(0x04 + relocCount * 8, (int)(dataToOffset[reloc.Symbol] + reloc.AddEnd));
+                    relocCount++;
+                }
+            }
+
+            function._s.SetReferenceStruct(0x04, relocationTable);
+            function._s.SetInt32(0x08, relocCount);
+            
+            return function;
         }
 
     }
