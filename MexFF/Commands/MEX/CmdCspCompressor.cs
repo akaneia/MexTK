9a585f303940a9a27fbd5528a74bb99cc0e03a70
Ploaj <ploaj_the_otter@yahoo.com>
2021-01-19 22:21:39 -0600

    threaded csp encoding

--- MexFF/Commands/MEX/CmdCspCompressor.cs
+++ MexFF/Commands/MEX/CmdCspCompressor.cs
@@ -1,5 +1,7 @@
-﻿using HSDRaw.MEX.Menus;
+﻿using HSDRaw.Common;
+using HSDRaw.MEX.Menus;
 using MexTK.Tools;
+using System.Threading;
 
 namespace MexTK.Commands.MEX
 {
@@ -17,11 +19,9 @@ namespace MexTK.Commands.MEX
                 return false;
 
             // parse percentage
-            if (!float.TryParse(args[2], out float percentage))
+            if (!float.TryParse(args[2], out percentage))
                 return false;
             percentage /= 100;
-
-
 
 
             var mexNode = node.Data as MEX_mexSelectChr;
@@ -32,20 +32,46 @@ namespace MexTK.Commands.MEX
 
                 for (int i = 0; i < textures.Length; i++)
                 {
-                    using (var csp = textures[i].ToBitmap())
-                    using (var shrink = ImageTools.ResizeImage(csp, (int)(csp.Width * percentage), (int)(csp.Height * percentage)))
-                    {
-                        textures[i].EncodeImageData(shrink.GetBGRAData(), shrink.Width, shrink.Height, HSDRaw.GX.GXTexFmt.CI8, HSDRaw.GX.GXTlutFmt.RGB5A3);
-                    }
+                    ThreadPool.QueueUserWorkItem(Process, textures[i]);
+                }
+
+                int toProcess = textures.Length;
+                using (ManualResetEvent resetEvent = new ManualResetEvent(false))
+                {
+                    for (int i = 0; i < textures.Length; i++)
+                        ThreadPool.QueueUserWorkItem(
+                            x =>
+                            {
+                                var texture = (HSD_TOBJ)x;
+                                using (var csp = texture.ToBitmap())
+                                using (var shrink = ImageTools.ResizeImage(csp, (int)(csp.Width * percentage), (int)(csp.Height * percentage)))
+                                {
+                                    texture.EncodeImageData(shrink.GetBGRAData(), shrink.Width, shrink.Height, HSDRaw.GX.GXTexFmt.CI8, HSDRaw.GX.GXTlutFmt.RGB5A3);
+                                }
+
+                                if (Interlocked.Decrement(ref toProcess) == 0)
+                                    resetEvent.Set();
+
+                            }, textures[i]);
+
+                    resetEvent.WaitOne();
                 }
 
                 mexNode.CSPMatAnim.TextureAnimation.FromTOBJs(textures, false);
             }
 
             file.TrimData();
-            file.Save(args.Length >= 3 ? args[3] : args[1]);
+            file.Save(args.Length > 3 ? args[3] : args[1]);
 
             return true;
+        }
+
+        private static float percentage = 1;
+
+        static void Process(object callback)
+        {
+            var textures = (HSD_TOBJ)callback;
+
         }
 
         public string Help()
